7bbc45aadecd26a05bd846adfec5eec1eafc1fe9
Michael Pezzi <mike@thespiral.ca>
2012-04-14 17:13:52 -0400

    added subreddits

--- js/script.js
+++ js/script.js
@@ -1,44 +1,66 @@
 $(function(){
 
-  var $posts = $('#posts');
-
-  // Pull a list of posts from reddit
-  $.reddit({ subreddit: window.location.hash.replace('#', ''), limit: 50 }, function(data){
-    if ( data.length > 0 ) {
-      // Append data to posts using the #post template.
-      $('#post').tmpl(data).appendTo($posts);
-
-      // Isotopize the posts.
-      $posts.isotope({
-        itemSelector : '.item'
-      });
-
-      // Append images to posts.
-      setTimeout(function(){
-        $.each(data, function(i, post){
-          if ( typeof post.url !== 'undefined' && post.url.indexOf('.jpg') != -1 ) {
-            $('<img class="image">').attr('src', post.url).load(function(){
-              $(this).prependTo('#' + post.id + ' p.description').slideDown(function(){
-                $posts.isotope('reLayout');
-              });
-            });
-          }
-        });
-      }, 2500);
-    }
-    else {
-      $('<div class="alert">').text('No posts found in subreddit.').appendTo($posts);
-    }
-  });
-
   // Fire events on specific media query layout changes.
   mql('all and (max-width: 480px)', reLayout);
   mql('all and (max-width: 768px)', reLayout);
   mql('all and (min-width: 980px)', reLayout);
   mql('all and (min-width: 1200px)', reLayout);
 
+  var $subreddits = $('#subreddits li a'),
+      $posts = $('#posts');
+
+  $(window).hashchange(function(){
+    load(location.hash.replace('#', ''), 50);
+  });
+
+  function load(subreddit, limit) {
+    clear();
+
+    $('#subreddits li').removeClass('active');
+    $('#subreddits li a[href="#' + subreddit.toLowerCase() + '"]')
+      .parent().addClass('active');
+
+    $('#subreddit span.subreddit').text(subreddit);
+
+    $.reddit({ subreddit: subreddit, limit: limit }, function(data){
+      if ( data.length > 0 ) {
+        // Append data to posts using the #post template.
+        $('#post').tmpl(data).appendTo($posts);
+
+        // Isotopize the posts.
+        $posts.isotope({
+          itemSelector : '.item'
+        });
+
+        // Append images to posts.
+        setTimeout(function(){
+          $.each(data, function(i, post){
+            if ( typeof post.url !== 'undefined' && post.url.indexOf('.jpg') != -1 ) {
+              $('<img class="image">').attr('src', post.url).wrap('<div class="image">').load(function(){
+                $(this).appendTo('#' + post.id + ' div.well').slideDown(function(){
+                  $posts.isotope('reLayout');
+                });
+              });
+            }
+          });
+        }, 2500);
+      }
+      else {
+        $('<div class="alert">').text('No posts found in subreddit.').appendTo($posts);
+      }
+    });
+  }
+
+  function clear() {
+    $posts.children().fadeOut(function(){
+      $posts.isotope('destroy').empty();
+    });
+  }
+
   function reLayout(mql) {
     $posts.isotope('reLayout');
   }
+
+  load(location.hash.replace('#', ''), 50);
 
 });
